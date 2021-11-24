547afc6ebeca4c6526bccb983e68a6b0fd950bdb
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-11-24 23:09:38 +0100

    Minor changes

--- notes/includes/db.php
+++ notes/includes/db.php
@@ -8,6 +8,6 @@ $dbname = "notes";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
     die("Connection failed " . mysqli_connect_error());
-} echo "Connection Successuf!";
+} //echo "Connection Successuf!";
 
 ?>
