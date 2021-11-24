60ebe3ebf6dc9eab40b2928de5d91ba92d844296
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-11-24 20:46:06 +0100

    Removed the port

--- notes/includes/db.php
+++ notes/includes/db.php
@@ -2,10 +2,10 @@
 $servername = "localhost";
 $username = "sqluser";
 $password = "sqlpass";
-$port = "33061";
+//$port = 33061;
 $dbname = "notes";
 
-$conn = mysqli_connect($servername, $username, $port, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
     die("Connection failed " . mysqli_connect_error());
 } echo "Connection Successuf!";
