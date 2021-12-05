3797400846437c52c957b36d84ed51f351e5cba5
Maduoma <18477889+maduoma@users.noreply.github.com>
2021-12-05 08:44:33 +0100

    Simple PHP Contact form

---
+++ SimpleContactForm/contact-form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>Contact Us</h2>
+    <p>Please fill in this form and send us.</p>
+    <form action="process-form.php" method="post">
+        <p>
+            <label for="inputName">Name:<sup>*</sup></label>
+            <input type="text" name="name" id="inputName">
+        </p>
+        <p>
+            <label for="inputEmail">Email:<sup>*</sup></label>
+            <input type="text" name="email" id="inputEmail">
+        </p>
+        <p>
+            <label for="inputSubject">Subject:</label>
+            <input type="text" name="subject" id="inputSubject">
+        </p>
+        <p>
+            <label for="inputComment">Message:<sup>*</sup></label>
+            <textarea name="message" id="inputComment" rows="5" cols="30"></textarea>
+        </p>
+        <input type="submit" value="Submit">
+        <input type="reset" value="Reset">
+    </form>
+</body>
+</html>
